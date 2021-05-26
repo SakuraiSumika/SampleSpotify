5532446dd676717efbbda149751cb4e72e1f0f3c
櫻井澄香 <sakuraisumika@sakuraisumikanoMacBook-Pro.local>
2021-05-26 13:28:55 +0900

    bbb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  root 'static_pages/home'
+  root 'static_pages#home'
+  resources :musics do
+       collection {get "search"}
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
